20e8ecdd5a256077866b70b68190b7ca0f547520
Mikhail Katychev <mkatych@gmail.com>
2023-05-12 15:49:53 -0400

    added bigerror lib.rs

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # bigerror
 `error BIGERROR NOT NULL`
+
+
+# example
+
+```rust
+use bigerror::{
+    to_report, from_report,
+    DbErrork, NetworkError,
+    NotFound, ParseError, TransactionError,
+    InvalidInput, Report
+};
+
+
+#[derive(Debug, thiserror::Error)]
+#[error("CliError")]
+pub struct CliError;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("{0:?}")]
+    Report(Report<CliError>),
+
+    #[error(transparent)]
+    TonicStatus(#[from] tonic::Status),
+}
+
+from_report!({
+    impl From<other_mod::Error as ToReport<_>>
+
+    impl From<DbError>
+    impl From<NetworkError>
+    impl From<NotFound>
+    impl From<ParseError>
+    impl From<TransactionError>
+
+    impl From<Report<InvalidInput>>
+    impl From<Report<NetworkError>>
+    impl From<Report<NotFound>>
+    impl From<Report<ParseError>>
+    impl From<Report<TransactionError>>
+
+    impl From<std::io::Error>
+
+    impl From<tonic::transport::Error as NetworkError>
+
+    impl From<std::num::ParseIntError as ParseError>
+    impl From<rust_decimal::Error as ParseError>
+    impl From<hex::FromHexError as ParseError>
+    impl From<std::num::TryFromIntError as ParseError>
+    impl From<uuid::Error as ParseError>
+
+    for Error::Report(CliError)
+});
+
+to_report!(impl ToReport<CliError> for Error::Report);
+```
---
+++ src/lib.rs
@@ -0,0 +1,432 @@
+use error_stack::{Context, Report, ResultExt};
+use std::{fmt, path::Path};
+use tracing::error;
+
+pub use error_stack;
+pub use thiserror;
+
+pub trait Reportable
+where
+    Self: Sized,
+{
+    fn report<E: Context>(e: E) -> Report<Self>;
+    fn attach<A>(value: A) -> Report<Self>
+    where
+        A: fmt::Display + fmt::Debug + Send + Sync + 'static;
+    fn attach_debug<A>(value: A) -> Report<Self>
+    where
+        A: fmt::Debug + Send + Sync + 'static;
+    fn with_kv<A>(key: &str, value: A) -> Report<Self>
+    where
+        A: fmt::Display + fmt::Debug + Send + Sync + 'static;
+}
+
+// TODO convert to #[derive(Reportable)]
+#[macro_export]
+macro_rules! reportable {
+    ($context:ident) => {
+        impl $crate::Reportable for $context {
+            #[track_caller]
+            fn report<E>(e: E) -> $crate::error_stack::Report<Self>
+            where
+                E: $crate::error_stack::Context,
+            {
+                $crate::error_stack::Report::new(e).change_context(Self)
+            }
+
+            #[track_caller]
+            fn attach<A>(value: A) -> $crate::error_stack::Report<Self>
+            where
+                A: fmt::Display + std::fmt::Debug + Send + Sync + 'static,
+            {
+                $crate::error_stack::Report::new(Self).attach_printable(value)
+            }
+
+            #[track_caller]
+            fn attach_debug<A>(value: A) -> $crate::error_stack::Report<Self>
+            where
+                A: std::fmt::Debug + Send + Sync + 'static,
+            {
+                $crate::error_stack::Report::new(Self).attach_printable(format!("{value:?}"))
+            }
+
+            #[track_caller]
+            fn with_kv<A>(key: &str, value: A) -> $crate::error_stack::Report<Self>
+            where
+                A: std::fmt::Display + std::fmt::Debug + Send + Sync + 'static,
+            {
+                use $crate::AttachExt;
+                $crate::error_stack::Report::new(Self).attach_kv(key, value)
+            }
+        }
+    };
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("NetworkError")]
+pub struct NetworkError;
+reportable!(NetworkError);
+
+#[derive(Debug, thiserror::Error)]
+#[error("ParseError")]
+pub struct ParseError;
+reportable!(ParseError);
+
+#[derive(Debug, thiserror::Error)]
+#[error("NotFound")]
+pub struct NotFound;
+reportable!(NotFound);
+
+#[derive(Debug, thiserror::Error)]
+#[error("DbError")]
+pub struct DbError;
+reportable!(DbError);
+
+#[derive(Debug, thiserror::Error)]
+#[error("MissingField")]
+pub struct MissingField(&'static str);
+
+#[derive(Debug, thiserror::Error)]
+#[error("ConversionError")]
+pub struct ConversionError;
+reportable!(ConversionError);
+
+#[derive(Debug, thiserror::Error)]
+#[error("InvalidInput")]
+pub struct InvalidInput;
+reportable!(InvalidInput);
+
+#[derive(Debug, thiserror::Error)]
+#[error("InvalidStatus")]
+pub struct InvalidStatus;
+reportable!(InvalidStatus);
+
+#[derive(Debug, thiserror::Error)]
+#[error("ConfigError")]
+pub struct ConfigError;
+reportable!(ConfigError);
+
+impl InvalidInput {
+    #[track_caller]
+    pub fn with_path(path: impl AsRef<Path>) -> Report<Self> {
+        let path = path.as_ref().display().to_string();
+        Report::new(Self).attach_kv("path", path)
+    }
+}
+
+impl ConversionError {
+    #[track_caller]
+    #[allow(dead_code)]
+    fn new<A>(from: A, to: A) -> Report<Self>
+    where
+        A: fmt::Display + fmt::Debug + Send + Sync + 'static,
+    {
+        Report::new(Self)
+            .attach_printable(format!("from: {from}"))
+            .attach_printable(format!("to: {to}"))
+    }
+}
+
+impl MissingField {
+    #[track_caller]
+    pub fn new(field: &'static str) -> Report<Self> {
+        Report::new(Self(field))
+    }
+}
+
+impl NotFound {
+    #[track_caller]
+    pub fn with_field(field: &'static str) -> Report<Self>
+where {
+        MissingField::new(field).change_context(Self)
+    }
+}
+
+pub trait AttachExt {
+    #[track_caller]
+    fn attach_kv<A>(self, key: &str, value: A) -> Self
+    where
+        A: fmt::Display + fmt::Debug + Send + Sync + 'static;
+    fn attach_kv_debug<A>(self, key: &str, value: A) -> Self
+    where
+        A: fmt::Debug + Send + Sync + 'static;
+}
+
+impl<C> AttachExt for error_stack::Report<C> {
+    #[track_caller]
+    fn attach_kv<A>(self, key: &str, value: A) -> Self
+    where
+        A: fmt::Display + fmt::Debug + Send + Sync + 'static,
+    {
+        self.attach_printable(format!("{key}: {value}"))
+    }
+
+    #[track_caller]
+    fn attach_kv_debug<A>(self, key: &str, value: A) -> Self
+    where
+        A: fmt::Debug + Send + Sync + 'static,
+    {
+        self.attach_printable(format!("{key}: {value:?}"))
+    }
+}
+
+impl<T, C> AttachExt for Result<T, Report<C>> {
+    #[track_caller]
+    fn attach_kv<A>(self, key: &str, value: A) -> Self
+    where
+        A: fmt::Display + fmt::Debug + Send + Sync + 'static,
+    {
+        self.attach_printable(format!("{key}: {value}"))
+    }
+
+    #[track_caller]
+    fn attach_kv_debug<A>(self, key: &str, value: A) -> Self
+    where
+        A: fmt::Debug + Send + Sync + 'static,
+    {
+        self.attach_printable(format!("{key}: {value:?}"))
+    }
+}
+
+// intended to be a quick passthrough for propagating errors to the message log
+// in a functional matter
+pub trait LogError<T, E>
+where
+    E: fmt::Debug,
+{
+    // swallows and logs error
+    fn log_err(self);
+    // swallows and logs error with attachment
+    fn log_attached_err<A>(self, attachment: A)
+    where
+        A: fmt::Debug + Send + Sync + 'static;
+    // logs error and forwards
+    fn and_log_err(self) -> Result<T, E>;
+    // logs error and forwards with attachment
+    fn and_attached_err<A>(self, attachment: A) -> Result<T, E>
+    where
+        A: fmt::Debug + Send + Sync + 'static;
+}
+
+impl<T, E> LogError<T, E> for Result<T, E>
+where
+    E: fmt::Debug,
+{
+    fn log_err(self) {
+        if let Err(e) = self {
+            error!(err = ?e);
+        }
+    }
+
+    fn log_attached_err<A>(self, attachment: A)
+    where
+        A: fmt::Debug + Send + Sync + 'static,
+    {
+        if let Err(e) = self {
+            error!(err = ?e, "{attachment:?}");
+        }
+    }
+
+    fn and_log_err(self) -> Result<T, E> {
+        if let Err(e) = &self {
+            error!(err = ?e);
+        }
+        self
+    }
+
+    fn and_attached_err<A>(self, attachment: A) -> Result<T, E>
+    where
+        A: fmt::Debug + Send + Sync + 'static,
+    {
+        if let Err(e) = &self {
+            error!(err = ?e, "{attachment:?}");
+        }
+        self
+    }
+}
+
+pub trait ToReport<T> {
+    #[track_caller]
+    fn to_report(self) -> Report<T>;
+
+    #[track_caller]
+    fn change_context<C: Context>(self, context: C) -> Report<C>
+    where
+        Self: Sized,
+    {
+        self.to_report().change_context(context)
+    }
+}
+
+pub trait FromReport<C> {
+    fn to_report(self) -> Report<C>;
+}
+
+/// USAGE:
+/// * `impl From<SomeError as ToReport<_> $(as $context:path)*> for OurError::Report(OurReportError)`
+///  - Implements `From<E> where E: ToReport<_>` for  errors that implement [`ToReport`]
+/// * impl From<Report<SomeError>>> for OurError::Report(TransactionError)
+///  - Implements `From<Report<SomeError>>` for `Report<OurReportError>`
+/// * `impl From<SomeError $(as $context:path)*> for OurError::Report(TransactionError)`
+///  - Implements `From<SomeError>` for `Report<OurReportError>`
+///  - Used to cast an error to `OurReportError`, can use `as` to do multiple `::from` conversion:
+///    `impl From<SomeError as MiddleError>` does a concrete implementation of the `impl` below where `E` is `SomeError`:
+///    `impl From<E> for OurError where E: Into<MiddleError> {}`
+#[macro_export]
+macro_rules! from_report {
+    // ------
+    // From<ToReport<_> as ctx>
+    ({ impl From<$from:path as ToReport<_> $(as $context:path)*> $($tail:tt)* }) => {
+        from_report!(@t[] @report_t[] @to_report[$from $(as $context)*,] $($tail)*);
+    };
+    // From<ToReport<_>>
+    ({ impl From<$from:path as ToReport<_>> $($tail:tt)* }) => {
+        from_report!(@t[] @report_t[] @to_report[$from,] $($tail)*);
+    };
+    // From<Report<T>>
+    ({ impl From<Report<$from:path>> $($tail:tt)* }) => {
+        from_report!(@t[] @report_t[$from] @to_report[] $($tail)*);
+    };
+    // From<T>
+    ({ impl From<$from:path $(as $context:path)*> $($tail:tt)*  }) => {
+        from_report!(@t[$from $(as $context)*,] @report_t[] @to_report[] $($tail)*);
+    };
+    // ------
+    // From<Report<T>>
+    (
+    @t[$($t:path $(as $t_context:path)*,)*]
+    @report_t[$($report_t:path)*]
+    @to_report[$($to_report:path $(as $to_context:path)*,)*]
+    impl From<Report<$from:path>> $($tail:tt)*) => {
+        from_report!(
+            @t[$($t $(as $t_context)*,)*]
+            @report_t[$($report_t)* $from]
+            @to_report[$($to_report $(as $to_context)*,)*]
+            $($tail)*
+        );
+    };
+    // From<ToReport<_> as ctx>
+    (
+    @t[$($t:path $(as $t_context:path)*,)*]
+    @report_t[$($report_t:path)*]
+    @to_report[$($to_report:path $(as $to_context:path)*,)*]
+    impl From<$from:path as ToReport<_> $(as $context:path)*> $($tail:tt)*) => {
+        from_report!(
+            @t[$($t $(as $t_context)*,)*]
+            @report_t[$($report_t)*]
+            @to_report[$($to_report $(as $to_context)*,)* $from $(as $context)*,]
+            $($tail)*
+        );
+    };
+    // From<ToReport<_>>
+    (
+    @t[$($t:path $(as $t_context:path)*,)*]
+    @report_t[$($report_t:path)*]
+    @to_report[$($to_report:path $(as $to_context:path)*,)*]
+    impl From<$from:path as ToReport<_>> $($tail:tt)*) => {
+        from_report!(
+            @t[$($t $(as $t_context)*,)*]
+            @report_t[$($report_t)*]
+            @to_report[$($to_report $(as $to_context)*,)* $from,]
+            $($tail)*
+        );
+    };
+    // From<T>
+    (
+    @t[$($t:path $(as $t_context:path)*,)*]
+    @report_t[$($report_t:path)*]
+    @to_report[$($to_report:path $(as $to_context:path)*,)*]
+    impl From<$from:path $(as $context:path)*> $($tail:tt)*) => {
+        from_report!(
+            @t[$($t $(as $t_context)*,)* $from $(as $context)*,]
+            @report_t[$($report_t)*]
+            @to_report[$($to_report $(as $to_context)*,)*]
+            $($tail)*
+        );
+    };
+    (
+    @t[$($t:path $(as $t_context:path)*,)*]
+    @report_t[$($report_t:path)*]
+    @to_report[$($to_report:path $(as $to_context:path)*,)*]
+    for $for:ident::$variant:ident($inner:path)) => {
+        // T
+        $(impl From<$t> for $for {
+            #[track_caller]
+            fn from(e: $t) -> Self {
+                let report = error_stack::Report::new(e)
+                $(.change_context($t_context))*
+                  .change_context($inner);
+                Self::$variant(report)
+            }
+        })*
+
+        // Report<T>
+        $(impl From<error_stack::Report<$report_t>> for $for {
+            #[track_caller]
+            fn from(r: error_stack::Report<$report_t>) -> Self {
+                Self::$variant(r.change_context($inner))
+            }
+        })*
+        // ToReport<_>
+        $(impl From<$to_report> for $for {
+            #[track_caller]
+            fn from(e: $to_report) -> Self {
+                use $crate::ToReport;
+                let report =  e.to_report()
+                $(.change_context($to_context))*
+                  .change_context($inner);
+                Self::$variant(report)
+            }
+        })*
+        // original
+        impl From<error_stack::Report<$inner>> for $for {
+            #[track_caller]
+            fn from(r: error_stack::Report<$inner>) -> Self {
+                Self::$variant(r)
+            }
+        }
+    };
+    (impl From<Report<$from:path>> for $for:ident::$variant:ident) => {
+        impl From<error_stack::Report<$from>> for $for {
+            #[track_caller]
+            fn from(r: error_stack::Report<$from>) -> Self {
+                Self::$variant(r)
+            }
+        }
+    };
+    (impl From<$from:path as ToReport<$context:path>> for $for:ident::$variant:ident) => {
+        impl From<$from> for $for {
+            #[track_caller]
+            fn from(e: $from) -> Self {
+                Self::$variant(<$from as $crate::error::ToReport<_>>::change_context(
+                    e, $context,
+                ))
+            }
+        }
+    };
+    (impl From<$from:path $(as $context:path)*> for $for:ident::$variant:ident) => {
+        impl From<$from> for $for {
+            #[track_caller]
+            fn from(e: $from) -> Self {
+                let report = error_stack::Report::new(e)
+                    $(.change_context($context))* ;
+                Self::$variant(report)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! to_report {
+    (impl ToReport<$reportable:path> for $for:ident::$variant:ident) => {
+        impl $crate::ToReport<$reportable> for $for {
+            fn to_report(self) -> $crate::error_stack::Report<$reportable> {
+                #[allow(unreachable_patterns)]
+                match self {
+                    Self::$variant(inner) => inner,
+                    _ => $crate::error_stack::Report::new(self).change_context($reportable),
+                }
+            }
+        }
+    };
+}
